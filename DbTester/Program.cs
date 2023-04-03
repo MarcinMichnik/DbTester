b8da3f2ad97e326b71822c1335c9c28b89a8784c
Marcin Michnik <marcin.michnik@o2.pl>
2023-04-03 20:19:31 +0200

    .

--- DbTester/Program.cs
+++ DbTester/Program.cs
@@ -1,11 +1,46 @@
 ﻿using DbTester.Statements;
 using Newtonsoft.Json.Linq;
+using System.Data;
+using System.Data.SqlClient;
 
 string jsonFilePath = @"..\..\..\Entries.json";
 string fullPath = Path.GetFullPath(jsonFilePath);
 string jsonString = File.ReadAllText(fullPath);
 JArray input = JArray.Parse(jsonString);
-
-CreateTable ct = new CreateTable("T_TEST_TABLE");
+string tableName = "T_TEST_TABLE";
+CreateTable ct = new CreateTable(tableName);
 string query = ct.FromJArray(input);
-Console.WriteLine(query);
+
+SqlConnection connection = new SqlConnection(
+    "Data Source=localhost;Initial Catalog=master;Integrated Security=True");
+connection.Open();
+SqlCommand command = new SqlCommand(query, connection);
+command.ExecuteNonQuery();
+
+SqlCommand command3 = new SqlCommand($"INSERT INTO {tableName} VALUES (1, 'John', 2100, CURRENT_TIMESTAMP)", connection);
+command3.ExecuteNonQuery();
+
+SqlCommand command2 = new SqlCommand($"SELECT * FROM {tableName}", connection);
+SqlDataReader reader = command2.ExecuteReader();
+
+while (reader.Read())
+{
+    ReadSingleRow((IDataRecord)reader);
+}
+reader.Close();
+
+SqlCommand command4 = new SqlCommand($"DROP TABLE {tableName}", connection);
+command4.ExecuteNonQuery();
+
+connection.Close();
+void ReadSingleRow(IDataRecord dataRecord)
+{
+    for (int i = 0; i < dataRecord.FieldCount; i++)
+    {
+        Console.Write(dataRecord[i]);
+        if (i != dataRecord.FieldCount - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+}
